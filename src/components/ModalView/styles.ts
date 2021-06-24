45a4084ae2cb9a3c75455ed4b02bfdb34d959af2
Soulsurfe <soulsurfe18@gmail.com>
2021-06-23 21:35:07 -0300

    aula-4/1

--- src/components/ModalView/styles.ts
+++ src/components/ModalView/styles.ts
@@ -1,22 +1,21 @@
-import { StyleSheet } from 'react-native';
-import { theme } from '../../global/styles/theme';
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 100
+    marginTop: 100,
   },
   overlay: {
     flex: 1,
-    backgroundColor: theme.colors.overlay
+    backgroundColor: theme.colors.overlay,
   },
   bar: {
     width: 39,
     height: 2,
     borderRadius: 2,
     backgroundColor: theme.colors.secondary30,
-    alignSelf: 'center',
+    alignSelf: "center",
     marginTop: 13,
-    marginBottom: 103,
-  }
+  },
 });
--- src/screens/Guilds/index.tsx
+++ src/screens/Guilds/index.tsx
@@ -1,45 +1,44 @@
-import React from 'react';
-import { View, FlatList } from 'react-native';
-import { GuildProps } from '../../components/Guild';
+import React from "react";
+import { View, FlatList } from "react-native";
+import { GuildProps } from "../../components/Guild";
 
-import { Guild } from '../../components/Guild';
-import { ListDivider } from '../../components/ListDivider';
+import { Guild } from "../../components/Guild";
+import { ListDivider } from "../../components/ListDivider";
 
-import { styles } from './styles';
+import { styles } from "./styles";
 
 type Props = {
   handleGuildSelect: (guild: GuildProps) => void;
-}
+};
 
-export function Guilds({ handleGuildSelect }: Props){
+export function Guilds({ handleGuildSelect }: Props) {
   const guilds = [
     {
-      id: '1',
-      name: 'Lendários',
-      icon: 'image.png',
-      owner: true
+      id: "1",
+      name: "Lendários",
+      icon: "image.png",
+      owner: true,
     },
     {
-      id: '2',
-      name: 'Galera do Game',
-      icon: 'image.png',
-      owner: true
-    }
+      id: "2",
+      name: "Galera do Game",
+      icon: "image.png",
+      owner: true,
+    },
   ];
 
   return (
     <View style={styles.container}>
-      <FlatList 
+      <FlatList
         data={guilds}
-        keyExtractor={item => item.id}
+        keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <Guild 
-            data={item} 
-            onPress={() => handleGuildSelect(item)}
-          />
-        )}    
+          <Guild data={item} onPress={() => handleGuildSelect(item)} />
+        )}
         showsVerticalScrollIndicator={false}
-        ItemSeparatorComponent={() => <ListDivider />}
+        ItemSeparatorComponent={() => <ListDivider isCentered />}
+        contentContainerStyle={{ paddingBottom: 68, paddingTop: 103 }}
+        ListHeaderComponent={() => <ListDivider isCentered />}
         style={styles.guilds}
       />
     </View>
